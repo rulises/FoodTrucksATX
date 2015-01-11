c99e283216b4cae9676227d0a2bb29325573ca39
Ulises Cardenas <ulises.card@gmail.com>
2015-01-11 15:23:18 -0600

    1

--- app.js
+++ app.js
@@ -30,11 +30,12 @@ var io = require('socket.io').listen(server);
 server.listen(80);
 
 io.on('connection', function (socket) {
+    io.set("transports", ["xhr-polling"]);
+    io.set("polling duration", 10);
     socket.on('send:coords', function (data) {
         socket.broadcast.emit('load:coords', data);
     });
 });
-
 
 
 app.use('/', routes);
