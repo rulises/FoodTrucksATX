23eab4124db2197541821dc510f1ed1b63de1a18
Ulises Cardenas <ulises.card@gmail.com>
2015-01-11 10:35:47 -0600

    1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+/* GET about page. */
+router.get('/about', function(req, res) {
+  res.render('about');
 });
 
 module.exports = router;
